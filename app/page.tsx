0cee4d88de490e0c3537981d746dd2062f044e43
Rodel Libed <libedrodel08@gmail.com>
2024-03-21 12:46:15 +0800

    added initial files

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,157 @@
-import Image from "next/image";
+"use client"
+import React, { useState } from "react";
+
+
+type TodoList = {
+   id:number
+   fname: string,
+   lname: string
+   isEditing?: boolean
+}
+
+type EditData = {
+ 
+   names: TodoList
+   onEdit: (data:TodoList) => void
+   
+}
+
+const data : TodoList[] = [
+  { id: 1, fname: "John", lname: "Doe", isEditing: false }
+]
 
 export default function Home() {
+
+  const [todoList, setTodoList] = useState(data)
+  const [inputValue, setInputValue] = useState({
+    fname: "", 
+    lname: ""
+  })
+
+  const [filter, setFilterValue] = useState("")
+  
+ 
+
+  let nextId = todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1
+
+
+    const handleOnChange1 = (e:React.ChangeEvent<HTMLInputElement>) => {
+       setInputValue({
+         ...inputValue,
+         fname: e.target.value
+       })
+    }
+    const handleOnChange2 = (e:React.ChangeEvent<HTMLInputElement>) => {
+      setInputValue(
+        {
+          ...inputValue,
+          lname: e.target.value
+        }
+      )
+   }
+
+    const handleAddData = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      setTodoList([...todoList, {id: nextId, fname: inputValue.fname, lname: inputValue.lname}])
+      setInputValue({...inputValue, fname: "", lname: ""})
+    
+    }
+
+    const handleDeleteData = (id:number) => {
+       setTodoList(todoList.filter((names)=> names.id !== id))
+    }
+
+    const handleEditData = (id:number) => {
+       setTodoList(todoList.map((names)=> {
+         return names.id === id ? {...names, isEditing: !names.isEditing} : names
+       }))
+    }
+
+    const handleSaveData = (data:TodoList) => {
+      setTodoList(todoList.map((todo)=> {
+         return todo.id === data.id ? {...todo, ...data, isEditing: false} : todo
+      }))
+    console.log(todoList); 
+    }
+
+    const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value.toLowerCase();
+      setFilterValue(value); 
+    };
+    
+   
+    const filteredTodoList = todoList.filter((todo) =>
+      todo.fname.toLowerCase().includes(filter) || todo.lname.toLowerCase().includes(filter)
+    );
+    
+  
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex min-h-screen flex-col items-center  p-24">
+       <h1 className="text-black text-2xl font-medium">TodoList with Filtering</h1>
+     
+    <form onSubmit={handleAddData} className="space-x-5">
+       <input onChange={handleOnChange1} value={inputValue.fname} type="text" className="border border-black" />
+       <input onChange={handleOnChange2} value={inputValue.lname} type="text" className="border border-black" />
+       <button className="text-black font-medium bg-green-500 py-1 px-2 rounded-full">ADD INFO</button>
+     </form>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+     
+      {/* Filter input field */}
+      <input
+        type="text"
+        value={filter}
+        onChange={handleFilterChange}
+        placeholder="Filter todo list..."
+        className="border border-black mt-5"
+      />
+      
+     
+      <div className="mt-10">
+        {filteredTodoList.map((names)=> {
+          return (
+            <div key={names.id}> 
+            {names.isEditing ? <Edit names={names} onEdit={handleSaveData} /> : <>
+              <p >{names.fname} {names.lname}</p>
+              <button onClick={()=>handleDeleteData(names.id)} className="text-black font-medium bg-red-500 py-1 px-2 rounded-full">DELETE</button>
+              <button onClick={()=>handleEditData(names.id)} className="text-black font-medium bg-green-500 py-1 px-2 rounded-full">EDIT</button></>}
+            </div>
+          )
+        })}
+         
+       </div>
+    
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
+}
+
+
+
+export const Edit = ({names,  onEdit}:EditData) =>{
+  const [currentValue, setCurrentValue] = useState({
+    id: names.id,
+    fname: names.fname,
+    lname: names.lname
+  })
+
+  const handleFnameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentValue({ ...currentValue, fname: e.target.value });
+  };
+
+  const handleLnameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentValue({ ...currentValue, lname: e.target.value });
+  };
+
+
+
+
+    return (
+      <>
+         <input onChange={handleFnameChange} value={currentValue.fname} type="text" className="border border-black" />
+         <input onChange={handleLnameChange}  value={currentValue.lname} type="text" className="border border-black" />
+         <button onClick={()=>onEdit(currentValue)}  className="text-black font-medium bg-green-500 py-1 px-2 rounded-full">SAVE</button>
+      </>
+    
+    )
 }
